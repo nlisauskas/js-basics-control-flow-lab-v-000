3c4759e71d3b680309fe648db24f0efa8c312182
Nick Lisauskas <nlisausk@alumni.nd.edu>
2018-03-08 12:23:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 // Write your code in this file!
 function scuberGreetingForFeet(distance) {
-  
+  if distance <= 400
+  return 'This one is on me!'
+  else if distance > 2000
+  return 'I will gladly take your thirty bucks.'
+  else if distance > 2500
+  return 'No can do.'
+  else 'Enter a valid distance.'
 };
+
+function 
