b3b11f8aabda29353417b725a90181b484d8c26c
Nick Lisauskas <nlisausk@alumni.nd.edu>
2018-03-08 12:32:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function scuberGreetingForFeet(distance) {
 };
 
 function ternaryCheckCity(city) {
-  city === 'NYC' ? return 'Ok, sounds good.' : return 'No go.';
+  return city === 'NYC' ?  'Ok, sounds good.' :  'No go.';
 };
 
 function switchOnCharmFromTip() {
