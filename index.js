28ff8fb6c2b9a33c7f9715ffcadbea00bd3db7f1
Nick Lisauskas <nlisausk@alumni.nd.edu>
2018-03-08 12:34:38 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Write your code in this file!
+function scuberGreetingForFeet(distance) {
+  if (distance <= 400) {
+  return 'This one is on me!'
+}  else if (distance > 2000 && distance < 2500){
+  return 'I will gladly take your thirty bucks.'}
+  else if (distance > 2500) {
+    return 'No can do.'
+  }
+  else {return 'Enter a valid distance.'}
+};
+
+function ternaryCheckCity(city) {
+  return city === 'NYC' ?  'Ok, sounds good.' :  'No go.';
+};
+
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case 'generous':
+    greeting = 'Thank you so much.';
+    break;
+    case 'not as generous':
+    greeting = 'Thank you.';
+    break;
+    default:
+    greeting = 'Bye.';
+    break;
+  }
+  return greeting
+}
